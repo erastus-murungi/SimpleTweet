ca5f6d4a464d27218d728651bc447077e65f5573
Erastus Murungi <erastusmurungi@fb.com>
2020-06-30 13:25:58 -0700

    rename package

---
+++ app/src/main/java/com/codepath/apps/erastustweet/TimelineActivity.java
@@ -0,0 +1,40 @@
+package com.codepath.apps.restclienttemplate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import okhttp3.Headers;
+
+public class TimelineActivity extends AppCompatActivity {
+    public static final String TAG = "TimelineActivity";
+    TwitterClient twitterClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timeline);
+
+        twitterClient = TwitterApp.getRestClient(this);
+        populateHomeTimeline();
+
+    }
+
+    private void populateHomeTimeline() {
+        twitterClient.getHomeTimeline(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.i(TAG, "Json Success!");
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.e(TAG, "Json Failure!" + response, throwable);
+            }
+        });
+    }
+}
